bbd60758a9c88f98c8485087f9dd9ef89edfe3e0
Josh Porter <j@prtr.dev>
2022-05-13 09:55:52 -0400

    refactor: change gui to alt

--- keyboards/crkbd/keymaps/jporter-dev/keymap.c
+++ keyboards/crkbd/keymaps/jporter-dev/keymap.c
@@ -9,7 +9,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO, KC_Q, KC_W, KC_E, KC_R, KC_T,                                   /* */  KC_Y, KC_U, KC_I, KC_O, KC_P, KC_NO,
         KC_NO, LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_G,   /* */  KC_H, RSFT_T(KC_J), RCTL_T(KC_K), RALT_T(KC_L), TD(TD_SEMI_QUOTE), KC_NO,
         KC_NO, KC_Z, KC_X, KC_C, KC_V,                                         /* */  KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_NO,
-                           TD(TD_ESC_TAB), LT(_NUM, KC_BSPC), LGUI_T(KC_SPC),  /* */  RGUI_T(KC_ENT), LT(_SYM, KC_DEL), RGUI_T(KC_GRAVE)
+                           TD(TD_ESC_TAB), LT(_NUM, KC_BSPC), LGUI_T(KC_SPC),  /* */  RGUI_T(KC_ENT), LT(_SYM, KC_DEL), RALT_T(KC_GRAVE)
     ),
 
     [_NUM] = LAYOUT_split_3x6_3(
